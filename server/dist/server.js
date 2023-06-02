23716c1347c6edd1b1e4dd8355e670ca46ae4c27
bofmar <bofmar@outlook.com>
2023-06-02 19:18:25 +0300

    Connect to MongoDB

--- server/dist/server.js
+++ server/dist/server.js
@@ -3,18 +3,22 @@ import path from 'path';
 import url from 'url';
 import dotenv from 'dotenv';
 import cors from 'cors';
-//import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-const MONGOURL = process.env.MONGOURL;
+const MONGOURI = process.env.MONGOURI;
 const runningMessage = `Listening for requests on port ${PORT}`;
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
+mongoose.connect(MONGOURI).then(_result => {
+    app.listen(PORT, () => console.log(runningMessage, `MONGOURI: ${MONGOURI}`, `filename: ${__filename}`, `dirname: ${__dirname}`));
+}).catch(error => {
+    console.log(error);
+});
 app.use(cors());
 // Say hi
 app.get('/', (_req, res) => {
     res.json({ message: 'Hello from the backend' });
 });
-app.listen(PORT, () => console.log(runningMessage, `MONGOURL: ${MONGOURL}`, `filename: ${__filename}`, `dirname: ${__dirname}`));
 //# sourceMappingURL=server.js.map
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,18 +3,23 @@ import path from 'path';
 import url from 'url';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
-//import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const MONGOURL = process.env.MONGOURL;
+const MONGOURI = process.env.MONGOURI;
 const runningMessage = `Listening for requests on port ${PORT}`; 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
+mongoose.connect(MONGOURI as string).then(_result => {
+	app.listen(PORT, () => console.log(runningMessage, `MONGOURI: ${MONGOURI}`, `filename: ${__filename}`, `dirname: ${__dirname}`));
+}).catch(error => {
+	console.log(error);
+});
 
 app.use(cors());
 
@@ -23,4 +28,3 @@ app.get('/', (_req: express.Request, res: express.Response) => {
 	res.json({ message: 'Hello from the backend' });
 });
 
-app.listen(PORT, () => console.log(runningMessage, `MONGOURL: ${MONGOURL}`, `filename: ${__filename}`, `dirname: ${__dirname}`));
