5ec75f5dec52f5663b2c49054e00c406f7d2f136
bofmar <bofmar@outlook.com>
2023-06-02 21:32:34 +0300

    Create the Console model

---
+++ server/dist/models/console.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { DateTime } from "luxon";
+const Schema = mongoose.Schema;
+const ConsoleSchema = new Schema({
+    name: { type: String, required: true, minLength: 3 },
+    developer_name: { type: String, required: true, minLength: 1 },
+    description: { type: String, required: true },
+    release_date: { type: Date, required: true },
+    discontinued_date: Date,
+    image: String
+});
+// Virtual for the url
+ConsoleSchema.virtual('url').get(function () {
+    return `/catalog/console/${this.id}`;
+});
+// Virtual for the formated date of release
+ConsoleSchema.virtual('release_date_formated').get(function () {
+    return DateTime.fromJSDate(this.release_date).toLocaleString(DateTime.DATE_MED);
+});
+// Virtual for the url
+ConsoleSchema.virtual('discontinued_date_formated').get(function () {
+    return this.discontinued_date ? DateTime.fromJSDate(this.discontinued_date).toLocaleString(DateTime.DATE_MED) : '';
+});
+const Console = mongoose.model('Console', ConsoleSchema);
+export default Console;
+//# sourceMappingURL=console.js.map
---
+++ server/src/models/console.ts
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { DateTime } from "luxon";
+
+const Schema = mongoose.Schema;
+
+interface IConsole {
+	name: string;
+	developer_name: string;
+	description: string;
+	url: string;
+	release_date: Date;
+	release_date_formated: string;
+	discontinued_date?: Date;
+	discontinued_date_formated: string;
+	image?: string;
+}
+
+const ConsoleSchema = new Schema<IConsole>({
+	name: { type: String, required: true, minLength: 3 },
+	developer_name: { type: String, required: true, minLength: 1 },
+	description: { type: String, required: true },
+	release_date: { type: Date, required: true },
+	discontinued_date: Date,
+	image: String
+});
+
+// Virtual for the url
+ConsoleSchema.virtual('url').get(function () {
+	return `/catalog/console/${this.id}`;
+});
+
+// Virtual for the formated date of release
+ConsoleSchema.virtual('release_date_formated').get(function () {
+	return DateTime.fromJSDate(this.release_date).toLocaleString(DateTime.DATE_MED);
+});
+
+// Virtual for the url
+ConsoleSchema.virtual('discontinued_date_formated').get(function () {
+	return this.discontinued_date ? DateTime.fromJSDate(this.discontinued_date).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+const Console = mongoose.model<IConsole>('Console', ConsoleSchema);
+
+export default Console;
