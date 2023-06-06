63936a085f87b719e094d142b64d75f0d2deca16
bofmar <bofmar@outlook.com>
2023-06-06 11:05:29 +0300

    Update GameCard to use the SERVER_URI constant

--- client/src/components/GameCard.tsx
+++ client/src/components/GameCard.tsx
@@ -1,14 +1,16 @@
 import { IGame } from "../types/types";
+import { SERVER_URI } from "../constats";
 
 interface IGameProp {
 	game: IGame
 }
 
 export default function GameCard({game}: IGameProp) {
+	const imageUrl = `${SERVER_URI}images/647e1b8a88dc8f9b21b026b7.jpeg`
 	return (
 		<div>
 			<h3>{game.title}</h3>
-			<img src='http://localhost:5000/images/647e1b8a88dc8f9b21b026b7.jpeg'/>
+			<img src={imageUrl}/>
 			<section>
 				{game.genres.map(genre => <p key={genre._id}>{genre.name}</p>)}
 			</section>
