8a8501486870abca7425ebdbf57f8a2c0a506d30
bofmar <bofmar@outlook.com>
2023-06-08 10:28:51 +0300

    Add notification toasts when the user creates a new genre

--- client/src/components/GenresPanel.tsx
+++ client/src/components/GenresPanel.tsx
@@ -3,6 +3,8 @@ import useFetch from "../hooks/useFetch";
 import { IGenre } from "../types/types";
 import GenreCard from "./GenreCard";
 import { FormEvent, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function GenrePanel() {
 	const url = `${SERVER_URI}catalog/genres`;
@@ -11,6 +13,8 @@ export default function GenrePanel() {
 
 	const handleSubmit = async (e: FormEvent) => {
 		e.preventDefault();
+
+		const loadToast = toast.loading('Please wait...');
 
 		const response = await fetch(url, {
 			method: 'POST',
@@ -22,11 +26,14 @@ export default function GenrePanel() {
 		});
 		// TODO: HANDLE THE TOASTS
 		if(response.status === 400){ // The genre already exists
+			toast.update(loadToast, { render: `${formData.name} already exists in the database`, type: 'warning', isLoading: false });
 			console.log('show toast');
 		} else if(response.status === 201) { // The genre was created successfully
+			toast.update(loadToast, { render: `${formData.name} was created`, type: 'success', isLoading: false });
 			console.log('show toast, refresh on closing the toast');
-			window.location.reload();
+			setTimeout(() => window.location.reload(), 5000);
 		} else { // something went wrong
+			toast.update(loadToast, { render: 'Something went wrong. Please try again later', type: 'error', isLoading: false });
 			console.log('show toast');
 		}
 
@@ -41,6 +48,7 @@ export default function GenrePanel() {
 			</form>
 			{loading && <p>'Loading....'</p>}
 			{genres && genres.map(genre => <GenreCard genre={genre} key={genre._id} />)}
+			<ToastContainer autoClose={5000} theme="dark"/>
 		</>
 	);
 }
