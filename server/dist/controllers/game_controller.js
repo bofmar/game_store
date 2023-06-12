bf6f197d411c900b1de62248d9595a2cc26058c9
bofmar <bofmar@outlook.com>
2023-06-12 13:49:40 +0300

    Finish the server side logic for creating a new game

--- server/dist/controllers/game_controller.js
+++ server/dist/controllers/game_controller.js
@@ -1,7 +1,7 @@
 import Game from "../models/game.js";
-import Genre from '../models/genre.js';
 import Console from '../models/console.js';
-import mongoose from 'mongoose';
+import Publisher from '../models/publisher.js';
+import Genre from '../models/genre.js';
 // Get all games
 export const game_get_all = async (_req, res) => {
     const allGames = await Game.find({}).populate('publisher').populate('genres').populate('consoles').exec();
@@ -19,16 +19,12 @@ export const game_get_detailed = async (req, res, next) => {
 };
 // POST new game
 export const game_post_new = async (req, res) => {
-    // BUGS
-    // NO GENRES ARE ADDED
-    // NO CONSOLES ARE ADDED
-    const publisherId = req.body.publisher;
-    const genreId = [req.body.genres];
-    const consolesId = [req.body.consoles];
-    const allGenres = await Genre.find({}).exec();
-    const allConsoles = await Console.find({}).exec();
-    //console.log(genreId);
-    //res.send('ok');
+    const publisherId = JSON.parse(req.body.publisher);
+    const genreId = [req.body.genres].map(g => JSON.parse(g)).flat();
+    const consolesId = [req.body.consoles].map(c => JSON.parse(c)).flat();
+    const publisher = await Publisher.findById(publisherId._id, '_id').exec();
+    const allGenres = await Genre.find({}, '_id').exec();
+    const allConsoles = await Console.find({}, '_id').exec();
     const game = new Game({
         _id: req.body._id,
         title: req.body.title,
@@ -36,9 +32,9 @@ export const game_post_new = async (req, res) => {
         description: req.body.description,
         copies_in_stock: parseInt(req.body.copies_in_stock),
         price: parseFloat(req.body.price),
-        publisher: new mongoose.Types.ObjectId(publisherId._id),
-        genres: allGenres.filter(g => genreId.includes({ _id: g._id })),
-        consoles: allConsoles.filter(c => consolesId.includes({ _id: c._id })),
+        publisher: publisher,
+        genres: allGenres.filter(genre => genreId.some(g => genre._id.equals(g._id))),
+        consoles: allConsoles.filter(con => consolesId.some(c => con._id.equals(c._id)))
     });
     // TODO SERVER SIDE DATA VALIDATION
     const gameExists = await Game.findOne({ title: req.body.title }).exec();
--- server/src/controllers/game_controller.ts
+++ server/src/controllers/game_controller.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import Game from "../models/game.js";
-import Genre from '../models/genre.js';
 import Console from '../models/console.js';
-import mongoose from 'mongoose';
+import Publisher from '../models/publisher.js';
+import Genre from '../models/genre.js';
 
 // Get all games
 export const game_get_all = async (_req: express.Request, res: express.Response): Promise<void> => {
@@ -24,18 +24,14 @@ export const game_get_detailed = async (req: express.Request, res: express.Respo
 
 // POST new game
 export const game_post_new = async (req: express.Request, res: express.Response): Promise<void> => {
-	// BUGS
-	// NO GENRES ARE ADDED
-	// NO CONSOLES ARE ADDED
-	const publisherId = req.body.publisher;
-	const genreId = [req.body.genres];
-	const consolesId = [req.body.consoles];
+	const publisherId = JSON.parse(req.body.publisher);
+	const genreId = [req.body.genres].map(g => JSON.parse(g)).flat();
+	const consolesId = [req.body.consoles].map(c => JSON.parse(c)).flat();
 
-	const allGenres = await Genre.find({}).exec();
-	const allConsoles = await Console.find({}).exec();
+	const publisher = await Publisher.findById(publisherId._id, '_id').exec();
+	const allGenres = await Genre.find({}, '_id').exec();
+	const allConsoles = await Console.find({}, '_id').exec();
 
-	//console.log(genreId);
-	//res.send('ok');
 	const game = new Game({ 
 		_id: req.body._id,
 		title: req.body.title,
@@ -43,9 +39,9 @@ export const game_post_new = async (req: express.Request, res: express.Response)
 		description: req.body.description,
 		copies_in_stock: parseInt(req.body.copies_in_stock),
 		price: parseFloat(req.body.price),
-		publisher: new mongoose.Types.ObjectId(publisherId._id),
-		genres: allGenres.filter(g => genreId.includes({_id: g._id})),
-		consoles: allConsoles.filter(c => consolesId.includes({_id: c._id})),
+		publisher: publisher,
+		genres: allGenres.filter(genre => genreId.some(g => genre._id.equals(g._id))),
+		consoles: allConsoles.filter(con => consolesId.some(c => con._id.equals(c._id)))
 	});
 
 	// TODO SERVER SIDE DATA VALIDATION
