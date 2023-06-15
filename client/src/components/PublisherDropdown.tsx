d688c5a4618014e11f6516bf0334c9a977819dd3
bofmar <bofmar@outlook.com>
2023-06-15 12:34:52 +0300

    Fix publisher dropdown not updating properly

--- client/src/components/PublisherDropdown.tsx
+++ client/src/components/PublisherDropdown.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { IGameForm, IPublisher } from "../types/types";
 
 interface IPubDropProps {
@@ -8,10 +8,14 @@ interface IPubDropProps {
 }
 
 export default function PublisherDropdown(props: IPubDropProps) {
+	const [value, setValue] = useState(props.game ? props.game.publisher._id : undefined);
 	return (
 		<div>
 			<label htmlFor="publisher">Publisher</label>
-			<select name='publisher' id='publisher' value={props.game ? props.game.publisher._id : undefined}required onChange={e => props.handlePubSelection(e)}>
+			<select name='publisher' id='publisher' value={value} required onChange={e => {
+				props.handlePubSelection(e);
+				setValue(e.target.value);
+			}}>
 				<option value=''>---Select One---</option>
 				{props.allPublishers.sort((p1,p2) => p1.name > p2.name ? 1 : -1 ).map(publisher => <option value={publisher._id} key={publisher._id} >{publisher.name}</option>)}
 			</select>
