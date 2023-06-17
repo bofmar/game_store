64c60912bce79b22dfae1b71cf5e2236a0bfa4b5
bofmar <bofmar@outlook.com>
2023-06-17 19:46:01 +0300

    Create list of game genres

---
+++ server/dist/populateDB_prod.js
@@ -0,0 +1,182 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import Game from "./models/game.js";
+import Publisher from "./models/publisher.js";
+import Console from "./models/console.js";
+import Genre from "./models/genre.js";
+;
+console.log('This script populates some test games, publishers, genres and consoles to the tests database.');
+dotenv.config();
+const MONGOURI = process.env.MONGO_PROD_URI;
+const genres = [];
+const publishers = [];
+const consoles = [];
+mongoose.set("strictQuery", false); // Prepare for Mongoose 7
+main().catch((err) => console.log(err));
+async function main() {
+    console.log("Debug: About to connect");
+    await mongoose.connect(MONGOURI);
+    console.log("Debug: Should be connected?");
+    await createGenres();
+    await createPublishers();
+    await createConsoles();
+    await createGames();
+    console.log("Debug: Closing mongoose");
+    mongoose.connection.close();
+}
+async function genreCreate(name) {
+    const genre = new Genre({ name: name });
+    await genre.save();
+    genres.push(genre);
+    console.log(`Added genre: ${name}`);
+}
+async function publisherCreate(name, date_founded, bio) {
+    let publisherDetail = { name: name, date_founded: date_founded, bio: bio };
+    const publisher = new Publisher(publisherDetail);
+    await publisher.save();
+    publishers.push(publisher);
+    console.log(`Added publisher: ${name}`);
+}
+async function consoleCreate(name, developer_name, description, release_date, discontinued_date) {
+    const consoleDetail = {
+        name: name,
+        developer_name: developer_name,
+        description: description,
+        release_date: release_date,
+        discontinued_date: discontinued_date,
+    };
+    const newConsole = new Console(consoleDetail);
+    await newConsole.save();
+    consoles.push(newConsole);
+    console.log(`Added console: ${developer_name} ${name}`);
+}
+async function gameCreate({ _id, title, release_date, description, copies_in_stock, price, publisher, genres, consoles }) {
+    const gameDetail = {
+        _id: _id,
+        title: title,
+        release_date: release_date,
+        description: description,
+        copies_in_stock: copies_in_stock,
+        price: price,
+        publisher: publisher,
+        genres: genres,
+        consoles: consoles,
+    };
+    const game = new Game(gameDetail);
+    await game.save();
+    console.log(`Added game: ${title}`);
+}
+async function createGenres() {
+    console.log("Adding genres");
+    await Promise.all([
+        genreCreate("Action"),
+        genreCreate("Beat 'em up"),
+        genreCreate("Hack and slash"),
+        genreCreate("Fighting"),
+        genreCreate("Platformer"),
+        genreCreate("Shooter"),
+        genreCreate("Survival"),
+        genreCreate("Battle Royale"),
+        genreCreate("Action adventure"),
+        genreCreate("Stealth"),
+        genreCreate("Adventure"),
+        genreCreate("Interactive fiction"),
+        genreCreate("Interactive movie"),
+        genreCreate("Visual novel"),
+        genreCreate("Gacha"),
+        genreCreate("Horror"),
+        genreCreate("Survival horror"),
+        genreCreate("Licensed"),
+        genreCreate("Masocore"),
+        genreCreate("MMO"),
+        genreCreate("RPG"),
+        genreCreate("Action RPG"),
+        genreCreate("Tactical RPG"),
+        genreCreate("JRPG"),
+        genreCreate("Sci-fi"),
+        genreCreate("Simulation"),
+        genreCreate("Construction and management"),
+        genreCreate("Life simulator"),
+        genreCreate("Sports"),
+        genreCreate("Vehicle"),
+        genreCreate("Strategy"),
+        genreCreate("4X"),
+        genreCreate("Auto battler"),
+        genreCreate("MOBA"),
+        genreCreate("RTS"),
+        genreCreate("TTS"),
+        genreCreate("Tower defense"),
+        genreCreate("Turn based strategy"),
+    ]);
+}
+async function createPublishers() {
+    console.log("Adding publishers");
+    await Promise.all([
+        publisherCreate('Activistion Blizzard', new Date('2008-07-10'), 'generic bio'),
+        publisherCreate('Nintendo', new Date('1967-07-10'), 'generic bio'),
+        publisherCreate('Sega', new Date('1973-07-10'), 'generic bio'),
+        publisherCreate('Atlus', new Date('1985-07-10'), 'generic bio'),
+    ]);
+}
+async function createGames() {
+    console.log("Adding Games");
+    let i = 1;
+    await Promise.all([
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-02'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[1]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[2]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[3]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[4]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[5]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[6]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[7]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[8]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-08-01'), description: 'generic game description', copies_in_stock: i, price: 69.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[1]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 29.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[2]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-06-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 19.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[1]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[1]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[1]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[2]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[2]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[1], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[1], genres: [genres[0], genres[2]], consoles: [consoles[2]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[2], genres: [genres[0], genres[2]], consoles: [consoles[1]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[2]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[3]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[4]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[5]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[1]], consoles: [consoles[6]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[7]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[1], genres: [genres[0], genres[2]], consoles: [consoles[8]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[0], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[3], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[3], genres: [genres[0], genres[2]], consoles: [consoles[0]] }),
+        gameCreate({ _id: `${i}`, title: `Test game ${i++}`, release_date: new Date('2008-07-01'), description: 'generic game description', copies_in_stock: i, price: 59.99, publisher: publishers[3], genres: [genres[0]], consoles: [consoles[0]] }),
+    ]);
+}
+async function createConsoles() {
+    console.log("Adding consoles");
+    await Promise.all([
+        consoleCreate('Switch', 'Nintendo', 'generic console description', new Date('2008-07-10'), new Date('20012-09-13')),
+        consoleCreate('PC', 'Various', 'generic console description', new Date('2008-07-10'), new Date('20012-09-13')),
+        consoleCreate('Saturn', 'Sega', 'generic console description', new Date('2008-07-10'), new Date('20012-09-13')),
+        consoleCreate('Gameboy', 'Nintendo', 'generic console description', new Date('2008-07-10'), new Date('20012-09-13')),
+        consoleCreate('Xbox', 'Microsoft', 'generic console description', new Date('2008-07-10'), new Date('20012-09-13')),
+        consoleCreate('Playstation 5', 'Sony', 'generic console description', new Date('2008-07-10'), new Date('20012-09-13')),
+        consoleCreate('Playstation', 'Sony', 'generic console description', new Date('2008-07-10'), new Date('20012-09-13')),
+        consoleCreate('Jaguar', 'Atari', 'generic console description', new Date('2008-07-10'), new Date('20012-09-13')),
+        consoleCreate('Super Nintendo', 'Nintendo', 'generic console description', new Date('2008-07-10'), new Date('20012-09-13')),
+    ]);
+}
+//# sourceMappingURL=populateDB_prod.js.map
