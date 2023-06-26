a4caa69eeef41694ac1c3223ba14d6e3511901c8
bofmar <bofmar@outlook.com>
2023-06-26 13:32:06 +0300

    Add required elements to the navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,11 +1,22 @@
 import { NavLink } from "react-router-dom";
+import logo from '../assets/images/gamesplanet-logo.png'
 
 export default function NavBar() {
 	return (
 		<nav>
-			<NavLink to='/'>Home</NavLink>
-			<NavLink to='/store'>Store</NavLink>
-			<NavLink to='/control-panel'>Control Panel</NavLink>
+			<section className="nav-image-section">
+				<NavLink to='/'><img src={logo} /></NavLink>
+			</section>
+			<section className="nav-search-section">
+			{/* TODO add functionality to the search bar*/}
+				<form className="nav-search-form">
+					<input type="text" placeholder="Search..."/>
+				</form>
+			</section>
+			<section className="nav-controls-section">
+				<NavLink to='/store'>Store</NavLink>
+				<NavLink to='/control-panel'>Control Panel</NavLink>
+			</section>
 		</nav>
 	);
 }
