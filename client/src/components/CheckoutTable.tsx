c465c575a0c0ea87311724ff477904857018f7d6
bofmar <bofmar@outlook.com>
2023-07-03 15:40:53 +0300

    Style the checkout page

--- client/src/components/CheckoutTable.tsx
+++ client/src/components/CheckoutTable.tsx
@@ -9,27 +9,28 @@ export default function CheckoutTable() {
 	return (
 		<>
 			{Cart && Cart.cartItems.length > 0 ?
-			<div>
+			<div className="checkout-wrapper">
+				<h2>Review your purchase</h2>
 				<table className="checkout-table">
 					{Cart.cartItems.map((item, index) => (
 						<tr key={index}>
 							<td>{item.title}</td>
 							<td>{item.publisher.name}</td>
 							<td>{item.price}€</td>
-							<td><button onClick={() => Cart.removeFromCart(index)}>Remove</button></td>
+							<td><button className='remove-button' onClick={() => Cart.removeFromCart(index)}>Remove</button></td>
 						</tr>
 					))}
 					<tr>
 						<td></td>
 						<td>Total:</td>
-						<td>{Cart.cartItems.reduce((total, item) => {
+						<td id='total-price'>{Cart.cartItems.reduce((total, item) => {
 								return total + item.price;
 							},0)}€</td>
 					</tr>
 				</table> 
 				<button className="orange-button">Procceed to checkout</button>
 			</div> :
-			<div>
+			<div className="checkout-wrapper">
 				<p>There are no items in your cart currently</p>
 				<button className="orange-button" onClick={() => navigate('/store')}>Back to the store</button>
 			</div>}
