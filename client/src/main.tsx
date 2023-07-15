4c0de1f818036bed05310291e0e272f2d843d5a1
bofmar <bofmar@outlook.com>
2023-07-15 22:26:12 +0300

    Change to hashrouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-	<BrowserRouter basename='/game_store'>
+	<HashRouter basename='/game_store'>
 		<App />
-	</BrowserRouter>
+	</HashRouter>
   </React.StrictMode>,
 )
